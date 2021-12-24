706ae0577d07f5ecc2ef75b1ce3a927475cb59ff
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-12-24 10:11:28 -0300

    Loading product data from order screen and selecting price

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import { Platform, ScrollView } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { Alert, Platform } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
 
+import { OrderNavigationProps } from '@src/@types/navigation';
 import { RadioButton } from '@components/RadioButton';
 import { ButtonBack } from '@components/ButtonBack';
 
@@ -22,14 +24,40 @@ import {
     Sizes,
     Title,
 } from './styles';
+import { ProductProps } from '@src/components/ProductCard';
+
+type PizzaResponse = ProductProps & {
+    prices_sizes: {
+        [key: string]: number;
+    }
+}
 
 export function Order() {
-    const [sizes, setSizes] = useState('');
+    const [ sizes, setSizes ] = useState('');
+    const [ pizza, setPizza ] = useState<PizzaResponse>({} as PizzaResponse);
+    const [ quantity, setQuantity ] = useState(0);
+    const [ tableNUmber, setTableNumber ] = useState('');
     const navigation = useNavigation();
+    const route = useRoute();
+    const { id } = route.params as OrderNavigationProps;
+
+    const amount = sizes ? pizza.prices_sizes[sizes] * quantity : '0,00';
 
     function handleGoBack() {
         navigation.goBack();
     }
+
+    useEffect(() => {
+        if(id) {
+            firestore()
+                .collection('pizzas')
+                .doc(id)
+                .get()
+            .then(response => setPizza(response.data() as PizzaResponse))
+            .catch(() => Alert.alert('Pedido', 'Não foi possível caregar o produto'));
+        }
+    },[id]);
+
     return (
         <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
             <ContentScroll>
@@ -40,10 +68,10 @@ export function Order() {
                     />
                 </Header>
 
-                <Photo source={{ uri: 'https://github.com/santosfael.png' }} />
+                <Photo source={{ uri: pizza.photo_url }} />
 
                 <Form>
-                    <Title>Nome da pizza</Title>
+                    <Title>{pizza.name}</Title>
                     <Label>Selecione o tamanho da pizza</Label>
 
                     <Sizes>
@@ -62,16 +90,16 @@ export function Order() {
                     <FormRow>
                         <InputGroup>
                             <Label>Número da mesa</Label>
-                            <Input keyboardType='numeric' />
+                            <Input keyboardType='numeric' onChangeText={setTableNumber} />
                         </InputGroup>
 
                         <InputGroup>
                             <Label>Quantidade</Label>
-                            <Input keyboardType='numeric' />
+                            <Input keyboardType='numeric' onChangeText={(value) => setQuantity(Number(value))} />
                         </InputGroup>
                     </FormRow>
 
-                    <Price>valor de R$ 00,00</Price>
+                    <Price>valor de R$ {amount}</Price>
                     <Button
                         title='Confirmar pedido'
                     />
